8fa00a253917a5de1ef046af3c8a1015c4f355e6
NarminGale <narmingale@gmail.com>
2021-06-14 22:06:36 +0400

    Created firbase.js file

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyA8-PQit6W8DxGzahK6B6OGi12OeYFENus',
+  authDomain: 'rsp-project-aa51e.firebaseapp.com',
+  projectId: 'rsp-project-aa51e',
+  storageBucket: 'rsp-project-aa51e.appspot.com',
+  messagingSenderId: '172246362365',
+  appId: '1:172246362365:web:ef6f0239cf1f26387ef694',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
